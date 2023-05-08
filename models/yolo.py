48558bbb0ea436f13d40060b52f4d6e5ae9ef685
ImGarbage <86345207+Tioacsolo@users.noreply.github.com>
2023-05-08 22:02:23 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -351,7 +351,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
                 args.insert(2, n)  # number of repeats
                 n = 1
         #FasterNeXt
-         elif m in [FasterNeXt]:
+        elif m in [FasterNeXt]:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
