a11ff423e39431df9aa22182215929dec494abd1
ImGarbage <86345207+Tioacsolo@users.noreply.github.com>
2023-05-08 16:11:00 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -350,6 +350,16 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             if m in [BottleneckCSP, C3, C3TR, C3Ghost, C3HB, C3RFEM, C3STR]:
                 args.insert(2, n)  # number of repeats
                 n = 1
+        #FasterNeXt
+         elif m in [FasterNeXt]:
+            c1, c2 = ch[f], args[0]
+            if c2 != no:  # if not output
+                c2 = make_divisible(c2 * gw, 8)
+            args = [c1, c2, *args[1:]]
+            if m in [FasterNeXt]:
+                args.insert(2, n)  # number of repeats
+                n = 1
+
         # add module research
         elif m in [CARAFE, SPPCSPC, SPPFCSPC, RepConv, BoT3, CA, CBAM, NAMAttention, GAMAttention, Involution, Stem, ResCSPC, ResCSPB, \
                    ResXCSPB, ResXCSPC, BottleneckCSPB, BottleneckCSPC,
